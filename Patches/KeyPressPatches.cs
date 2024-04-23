2bbde7c037d8b9a64abd4f5617a1321b72354f62
Crafterbot <118089496+CrafterBotOfficial@users.noreply.github.com>
2024-04-23 15:37:31 -0400

    Fixed double clicking issue using Harmony's Transpiler feature

--- Patches/KeyPressPatches.cs
+++ Patches/KeyPressPatches.cs
@@ -1,6 +1,10 @@
-﻿using GorillaNetworking;
+﻿using ComputerPlusPlus.Tools;
+using GorillaNetworking;
 using HarmonyLib;
-using Photon.Pun;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
 using UnityEngine;
 
 namespace ComputerPlusPlus.Patches
@@ -14,5 +18,26 @@ namespace ComputerPlusPlus.Patches
                 return;
             ComputerManager.Instance.OnKeyPressed(__instance);
         }
+
+#if DEBUG
+        [HarmonyDebug]
+#endif
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = instructions.ToList();
+
+            int startIndex = codes.FindIndex(code => code.opcode == OpCodes.Callvirt && code.operand is MethodBase method && method.Name == "Invoke");
+            const int removeInstructionCount = 4; // exclusive
+            codes.RemoveRange(startIndex - removeInstructionCount + 1, removeInstructionCount);
+
+            return codes;
+        }
     }
 }
+/*
+//         GameEvents.OnGorrillaKeyboardButtonPressedEvent.Invoke(this.Binding);
+IL_0018: ldsfld    class [UnityEngine.CoreModule]UnityEngine.Events.UnityEvent`1<valuetype GorillaNetworking.GorillaKeyboardBindings> GameEvents::OnGorrillaKeyboardButtonPressedEvent
+IL_001D: ldarg.0
+IL_001E: ldfld     valuetype GorillaNetworking.GorillaKeyboardBindings GorillaNetworking.GorillaKeyboardButton::Binding
+IL_0023: callvirt  instance void class [UnityEngine.CoreModule]UnityEngine.Events.UnityEvent`1<valuetype GorillaNetworking.GorillaKeyboardBindings>::Invoke(!0)
+*/
--- PluginInfo.cs
+++ PluginInfo.cs
@@ -7,6 +7,6 @@
     {
         public const string GUID = "com.kylethescientist.gorillatag.computerplusplus";
         public const string Name = "Computer++";
-        public const string Version = "1.0.2"; 
+        public const string Version = "1.0.2.1"; 
     }
 }
